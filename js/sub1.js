cc52c5be24d65f189d5295c4bc17b0cda0fb8b99
gogyounglim <rhrudfla12445@naver.com>
2022-07-08 17:05:58 +0900

    20220708

---
+++ js/sub1.js
@@ -0,0 +1,16 @@
+
+// $(document).ready(function() {
+//     $('#subline').each( function() {
+            
+//         var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+//         var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+//         if( bottom_of_window > bottom_of_element ){
+//             // $('#subline div:nth child(1)').animate({'width':'85%'},1000);  
+//             $('#subline div:nth child(1)').css('width','85%');
+//         }
+    
+//     }); 
+// });
+
+
